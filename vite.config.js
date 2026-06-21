f52426f7065ada0ba72eb7f6cf7d57594285605c
Tedy Baisac <baisactedy2@gmail.com>
2026-06-21 10:12:34 +0800

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/thesis-dashboard/', // FIXED: Force Vite to map paths to your subfolder!
 })
